dfdc1de20f917e9d8ec290dc3a24eab18790c424
Subham Shrestha <subham.hero01@gmail.com>
2024-11-05 18:28:26 +0545

    tab and spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,8 +18,8 @@ end)
 vim.opt.breakindent = true
 
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
 vim.opt.virtualedit = "block"
 
