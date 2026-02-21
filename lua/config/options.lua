66821a18a275a2c90ac2b9c7790037b4621a1b77
Subham Shrestha <subham.hero01@gmail.com>
2026-02-21 12:46:17 +0545

    added opencode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -73,3 +73,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- Enable autoread and set up checking triggers
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+	command = "if mode() != 'c' | checktime | endif",
+	pattern = "*",
+})
