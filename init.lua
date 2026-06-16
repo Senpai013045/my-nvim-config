f06fbf53f88e5a8f1397821bb46149b4fe293cfa
Subham Shrestha <59314575+Senpai013045@users.noreply.github.com>
2026-06-16 13:20:20 +0545

    clipboard

--- init.lua
+++ init.lua
@@ -8,8 +8,7 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.mouse = 'a'
 vim.o.showmode = false
--- Defer to avoid startup-time cost
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+vim.o.clipboard = 'unnamedplus'
 vim.o.breakindent = true
 vim.o.expandtab = true
 vim.o.tabstop = 2
